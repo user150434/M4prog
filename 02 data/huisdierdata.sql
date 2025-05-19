17c8d23909b51cffa57e78f3fb630a43b57bc54e
user150434 <o.boudaati@gmail.com>
2025-05-19 15:06:07 +0200

    opdracht 3 en 4

--- 02 data/huisdierdata.sql
+++ 02 data/huisdierdata.sql
@@ -3,5 +3,5 @@ INSERT INTO `huisdieren` (`id`, `naam`, `eigennaar`, `typendier`, `leeftijd`, `g
 (2, 'blonker', 'jesper', 'kat', 4, '2021-05-11'),
 (3, 'blinky', 'bram', 'hond', 4, '2021-05-11'),
 (4, 'blonker', 'jesper', 'kat', 4, '2021-05-11'),
-(5, 'blinky', 'bram', 'hond', 4, '2021-05-11');
+(5, 'blinky', 'bram', 'hond', 4, '2021-05-11'),
 (6, 'blonker', 'jesper', 'kat', 4, '2021-05-11');
